44a1bcea0001a80a6ad54228233c62054e2a206b
Ashiqul Islam Emu <ashiqulemu.jpi@gmail.com>
2021-02-21 23:13:10 +0600

    last commit

--- resources/views/admin/includes/side-bar.blade.php
+++ resources/views/admin/includes/side-bar.blade.php
@@ -1,15 +1,9 @@
 <div class="navbar-default sidebar" role="navigation">
     <div class="sidebar-nav navbar-collapse">
         <ul class="nav" id="side-menu">
-
             <li>
                 <a href="{{url('/admin/dashboard')}}"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
             </li>
-           
-            
-          
-            
-           
             <li>
                 <a href="#"><i class="fa fa-certificate"></i> CMS<span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level">
@@ -20,39 +14,23 @@
                         <a href="{{url('/admin/cms')}}">Manage CMS</a>
                     </li>
                 </ul>
-                <!-- /.nav-second-level -->
             </li>
             <li>
-                <a href="{{url('/admin/quiz')}}"><i class="fa fa-book"></i>Quiz<span class="fa arrow"></span></a>
-                {{--<ul class="nav nav-second-level">--}}
-                    {{--<li>--}}
-                        {{--<a href="{{url('/admin/quiz/create')}}">Add Quiz</a>--}}
-                    {{--</li>--}}
-                    {{--<li>--}}
-                        {{--<a href="{{url('/admin/quiz')}}">Manage quiz</a>--}}
-                    {{--</li>--}}
-                {{--</ul>--}}
-                <!-- /.nav-second-level -->
+                <a href="{{url('/admin/quiz')}}"><i class="fa fa-book"></i> Quiz </a>
             </li>
-
             <li>
-                <a href="#"><i class="fa fa-certificate"></i>Quiz Head<span class="fa arrow"></span></a>
+                <a href="#"><i class="fa fa-certificate"></i> Quiz Head<span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level">
                     <li>
-                        <a href="{{url('/admin/quizhead/create')}}">Add Quizhead</a>
+                        <a href="{{url('/admin/quizhead/create')}}"> Add Quizhead</a>
                     </li>
                     <li>
-                        <a href="{{url('/admin/quizhead')}}">Manage Quizhead</a>
+                        <a href="{{url('/admin/quizhead')}}"> Manage Quizhead</a>
                     </li>
                 </ul>
-                <!-- /.nav-second-level -->
             </li>
-           
-
-
         </ul>
     </div>
-    <!-- /.sidebar-collapse -->
 </div>
-<!-- /.navbar-static-side -->
+
 
--- resources/views/admin/pages/quiz/dasboard.blade.php
+++ resources/views/admin/pages/quiz/dasboard.blade.php
@@ -10,97 +10,83 @@
                 <h4 class="well well-sm">Create Quiz</h4>
                 <div class="panel">
                     <div class="dashboard-container">
-                        <form action="{{ route('quiz.create') }}" method="POST" enctype="multipart/form-data" >
+                        <form action="{{ route('quiz.create') }}" method="POST" enctype="multipart/form-data">
                             {{ csrf_field() }}
-                            <div >
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <input type="text" name="quiz" placeholder="New Quiz Name" class="form-control"
-                                               required>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <input type="text" class="form-control"
-                                               id="datetimepicker"
-                                               name="expiry_date"
-                                               placeholder="expiry_date"
-                                        />
-
-                                    </div>
-                                    <br><br>
-
-                                </div>
-                            </div>
 
                             <div class="row">
-                                <div class="col-md-6">
-                                    <select class="js-example-basic-multiple form-control"
+                                <div class="col-md-4">
+                                    <input type="text" name="quiz" placeholder="New Quiz Name" class="form-control"
+                                           required>
+                                </div>
+                                <div class="col-md-4">
+                                    <input type="text" class="form-control"
+                                           id="datetimepicker"
+                                           name="expiry_date"
+                                           placeholder="expiry_date"
+                                    />
+
+                                </div>
+                                <div class="col-md-4">
+                                    <select width="100%" class=" form-control" style="outline: 0"
                                             name="quiz_type" id="select1">
                                         <option value="">Select Quiz Type</option>
                                         <option value="First"
-                                        @if (old('quiz_type') == 'First') {{ 'selected' }} @endif>First</option>
+                                        @if (old('quiz_type') == 'First') {{ 'selected' }} @endif>First
+                                        </option>
 
                                         <option value="Next"
-                                        @if (old('quiz_type') == 'Next') {{ 'selected' }} @endif>Next</option>
+                                        @if (old('quiz_type') == 'Next') {{ 'selected' }} @endif>Next
+                                        </option>
 
                                     </select>
                                 </div>
-                                <div class="col-md-6">
-
-                                </div>
-
                             </div>
-                            <br>
-                                    <button type="submit" class="btn btn-primary  btn-sm btn-success">Create</button>
-
-
-
-
+                            <button type="submit" class="btn btn-primary  btn-sm btn-success mt-2">Create</button>
                         </form>
                     </div>
                 </div>
 
                 <div class="panel">
                     <h4 class="well well-sm">Quiz List</h4>
+                    @if(isset($first) && isset($next))
 
-                        @if(isset($first) && isset($next))
-
-                            <div class="header">
-                                <div class="list-group">
-                                    <div class="list-group-item list-group-item-action">
-                                        <span>{{ $first->quiz }}</span>
-                                        <div style=" display: flex;    justify-content: center; margin-top: -5px;   align-items: center;"
-                                             class="pull-right">
-                                            <a class="btn btn-warning btn-sm" style="margin-right:5px;" role="button"
-                                               href="{{ route('quiz.editForm', ['id' => $first->id]) }}">Edit Quiz</a>
-                                            <form method="POST"
-                                                  action="{{ route('quiz.delete', ['id' => $first->id]) }}">
-                                                {{ csrf_field() }}
-                                                {{ method_field('DELETE') }}
-                                                <button class="btn btn-danger btn-sm" type="submit">Delete</button>
-                                            </form>
-                                        </div>
+                        <div class="header">
+                            <div class="list-group">
+                                <div class="list-group-item list-group-item-action">
+                                    <span>{{ $first->quiz }}</span>
+                                    <div style=" display: flex;    justify-content: center; margin-top: -5px;   align-items: center;"
+                                         class="pull-right">
+                                        <a class="btn btn-warning btn-sm" style="margin-right:5px;" role="button"
+                                           href="{{ route('quiz.editForm', ['id' => $first->id]) }}">Edit Quiz</a>
+                                        <form method="POST"
+                                              action="{{ route('quiz.delete', ['id' => $first->id]) }}">
+                                            {{ csrf_field() }}
+                                            {{ method_field('DELETE') }}
+                                            <button class="btn btn-danger btn-sm" type="submit">Delete</button>
+                                        </form>
                                     </div>
-                                    <div class="list-group-item list-group-item-action">
-                                        <span>{{ $next->quiz }}</span>
-                                        <div style=" display: flex;    justify-content: center; margin-top: -5px;   align-items: center;"
-                                             class="pull-right">
-                                            <a class="btn btn-warning btn-sm" style="margin-right:5px;" role="button"
-                                               href="{{ route('quiz.editForm', ['id' => $next->id]) }}">Edit Quiz</a>
-                                            <form method="POST"
-                                                  action="{{ route('quiz.delete', ['id' => $next->id]) }}">
-                                                {{ csrf_field() }}
-                                                {{ method_field('DELETE') }}
-                                                <button class="btn btn-danger btn-sm" type="submit">Delete</button>
-                                            </form>
-                                        </div>
+                                </div>
+                                <div class="list-group-item list-group-item-action">
+                                    <span>{{ $next->quiz }}</span>
+                                    <div style=" display: flex;    justify-content: center; margin-top: -5px;   align-items: center;"
+                                         class="pull-right">
+                                        <a class="btn btn-warning btn-sm" style="margin-right:5px;" role="button"
+                                           href="{{ route('quiz.editForm', ['id' => $next->id]) }}">Edit Quiz</a>
+                                        <form method="POST"
+                                              action="{{ route('quiz.delete', ['id' => $next->id]) }}">
+                                            {{ csrf_field() }}
+                                            {{ method_field('DELETE') }}
+                                            <button class="btn btn-danger btn-sm" type="submit">Delete</button>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
+                        </div>
 
 
                     @endif
                 </div>
-                <!---------------->
+
                 <div class="panel ">
                     <h4 class="well well-sm">Winner Prize</h4>
 
@@ -122,7 +108,6 @@
                             </div>
 
 
-
                         </form>
                     </div>
                     <br><br>
@@ -135,35 +120,41 @@
 
                             <div class="row">
 
-                                <form method="POST" action="{{ route('prize.prizeupdate', ['prize' => $record[$key]->id]) }}">
+                                <form method="POST"
+                                      action="{{ route('prize.prizeupdate', ['prize' => $record[$key]->id]) }}">
                                     {{ csrf_field() }}
-                                <div class="col-md-4">
-                                    <div class="form-group" style=" display: flex;">
-                                        <label>Score: </label>
-                                        <input type="number" value="{{$record[$key]->score}}" name="score"
-                                               class="form-control">
+                                    <div class="col-md-4">
+                                        <div class="form-group" style=" display: flex;">
+                                            <label>Score: </label>
+                                            <input type="number" value="{{$record[$key]->score}}" name="score"
+                                                   class="form-control">
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="form-group" style=" display: flex;">
-                                        <label>Prize: </label>
-                                        <input type="number" value="{{$record[$key]->gift}}" name="prize"
-                                               class="form-control">
+                                    <div class="col-md-4">
+                                        <div class="form-group" style=" display: flex;">
+                                            <label>Prize: </label>
+                                            <input type="number" value="{{$record[$key]->gift}}" name="prize"
+                                                   class="form-control">
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="col-md-2" style=" display: flex;">
+                                    <div class="col-md-2" style=" display: flex;">
 
 
-                                        <button class="btn btn-warning btn-sm" style="margin-right:5px;" role="button"  type="submit"> Update quiz</button>
+                                        <button class="btn btn-warning btn-sm" style="margin-right:5px;" role="button"
+                                                type="submit"> Update quiz
+                                        </button>
 
 
-                                </div>
+                                    </div>
                                 </form>
                                 <div class="col-md-2">
-                                    <form method="POST" action="{{ route('prize.delete', ['prize' => $record[$key]->id]) }}">
+                                    <form method="POST"
+                                          action="{{ route('prize.delete', ['prize' => $record[$key]->id]) }}">
                                         {{ csrf_field() }}
                                         {{ method_field('DELETE') }}
-                                        <button class="btn  btn-sm btn-danger text-danger"  title="Delete Prize" type="submit">Delete Prize</button>
+                                        <button class="btn  btn-sm btn-danger text-danger" title="Delete Prize"
+                                                type="submit">Delete Prize
+                                        </button>
                                     </form>
                                 </div>
                             </div>
@@ -178,7 +169,6 @@
                 </div>
 
 
-
             </div>
         </div>
     </div>
@@ -187,4 +177,10 @@
 @section('scripts')
 
 
-    @endsection
+@endsection
+
+<style>
+    .select2{
+        width: 100%;
+    }
+</style>
